c5fb3d85e36f774bb405c6e55e9c9ec0c0c1a80b
JMatachun468 <jared.matachun@aim.com>
2021-04-13 16:14:43 -0400

    test

--- Assets/Scripts/UI Script/ActionBarSlot.cs
+++ Assets/Scripts/UI Script/ActionBarSlot.cs
@@ -11,21 +11,51 @@ public class ActionBarSlot : MonoBehaviour, IDragHandler , IBeginDragHandler, IE
     private PlayerPawn player;
     private GraphicRaycaster raycaster;
     public SpellScriptableObject spellInSlot;
+    private RectTransform rt;
+    [SerializeField]
+    private GameObject draggingIcon;
+    public Vector3 startingPos;
+    public Vector2 mousePos;
     private bool dragging;
+    public Image panel;
+    private ActionBarSlot lastDraggedOverSlot;
     void Start()
     {
         player = GetComponentInParent<PlayerPawn>();
         raycaster = GetComponentInParent<GraphicRaycaster>();
+        rt = GetComponent<RectTransform>();
         gameObject.GetComponent<Image>().sprite = spellInSlot.abilitySprite;
+        startingPos = rt.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(dragging)
+        {
+            Dragging();
+        }
+        if (spellInSlot == null)
+        {
+            Color32 darkGrey = new Color32(133, 133, 133, 255);
+            ColorBlock temp = new ColorBlock();
+            temp.normalColor = darkGrey;
+            temp.highlightedColor = darkGrey;
+            temp.pressedColor = darkGrey;
+            temp.selectedColor = darkGrey;
+            temp.disabledColor = darkGrey;
+            temp.colorMultiplier = 1f;
+            gameObject.GetComponent<Button>().colors = temp;
+        }
+        else
+        {
+            ColorBlock temp = ColorBlock.defaultColorBlock;
+            temp.pressedColor = new Color32(85, 85, 85, 255);
+            gameObject.GetComponent<Button>().colors = temp;
+        }
     }
 
-    public void updateIcon()
+    public void updateIcon() //used for updating icon after moving an ability around
     {
         if(spellInSlot == null)
         {
@@ -44,17 +74,64 @@ public class ActionBarSlot : MonoBehaviour, IDragHandler , IBeginDragHandler, IE
         player.SpellRestrictionCheck(spellInSlot, player.target);
     }
 
+    public void Dragging()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        pointerData.position = Mouse.current.position.ReadValue();
+        raycaster.Raycast(pointerData, results);
+
+        bool foundSlot = false;
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.GetComponent<ActionBarSlot>())
+            {
+                foundSlot = true;
+                if (lastDraggedOverSlot != null)
+                {
+                    lastDraggedOverSlot.panel.color = Color.black;
+                }
+
+                lastDraggedOverSlot = result.gameObject.GetComponent<ActionBarSlot>();
+                lastDraggedOverSlot.panel.color = new Color32(255, 254, 183, 255);
+            }
+        }
+
+        Debug.Log(foundSlot);
+        if (!foundSlot)
+        {
+            if (lastDraggedOverSlot != null)
+            {
+                lastDraggedOverSlot.panel.color = Color.black;
+            }
+        }
+
+
+
+        draggingIcon.transform.position = Mouse.current.position.ReadValue();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (spellInSlot == null) return;
+        dragging = true;
         player.draggingAbility = true;
-        Debug.Log("start of drag");
+        gameObject.GetComponent<Image>().sprite = null;
+
+        draggingIcon = Instantiate(new GameObject("draggingIcon"), GetComponentInParent<Canvas>().transform);
+        draggingIcon.AddComponent<Image>();
+        draggingIcon.GetComponent<Image>().sprite = spellInSlot.abilitySprite;
+        draggingIcon.GetComponent<Image>().raycastTarget = false;
+        draggingIcon.GetComponent<RectTransform>().sizeDelta = new Vector2(75,75);
+
     }
+
+
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("end of drag");
-
+        if (spellInSlot == null) return;
         PointerEventData pointerData = new PointerEventData(EventSystem.current);
         List<RaycastResult> results = new List<RaycastResult>();
 
@@ -77,6 +154,7 @@ public class ActionBarSlot : MonoBehaviour, IDragHandler , IBeginDragHandler, IE
 
                 break;
             }
+
         }
 
         if(results.Count == 0)
@@ -84,7 +162,13 @@ public class ActionBarSlot : MonoBehaviour, IDragHandler , IBeginDragHandler, IE
             spellInSlot = null;
             updateIcon();
         }
+
+        lastDraggedOverSlot.panel.color = Color.black;
+        rt.transform.localPosition = startingPos;
+        Destroy(draggingIcon);
         player.draggingAbility = false;
+        dragging = false;
+        updateIcon();
     }
 
     public void OnDrag(PointerEventData eventData)
